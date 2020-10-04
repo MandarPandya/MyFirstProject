8ec65366cb29d5c33c04c587496f316c6d26f659
Mandar <pandya.mandar@gmail.com>
2020-10-04 12:24:59 -0600

    commited on 4oct2020

--- Program.cs
+++ Program.cs
@@ -4,20 +4,11 @@ namespace MyFirstProject
 {
     class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
-            for(int i = 1; i <= 100; i++){
-                string result = i + " ";
-
-                if (i % 3 == 0) {
-                    result += "Fizz";
-                }
-
-                if (i % 5 == 0) {
-                    result += "Buzz";
-                }
-                Console.WriteLine(result);
-            }
+           
+           TicketPricingExtended.extended();
+            
         }
     }
 }
---
+++ TicketPricing.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyFirstProject
+{
+    class TicketPricing
+    {
+       public static void getPrice()
+        {
+           
+           Console.WriteLine("Please enter age:");
+           string userInput = Console.ReadLine();
+           int i = Convert.ToInt32 (userInput);
+           if (i<=0 || i>130) {
+               Console.WriteLine ("Invalid Input");
+           }
+           else if (i>=0 && i<=5) {
+               Console.WriteLine ("Price is: Free");
+           }
+           else if (i>5 && i<=14) {
+               Console.WriteLine ("Price is: $7.99");
+           }
+           else if (i>14 && i<=65) {
+               Console.WriteLine ("Price is: $11.99");
+           }
+           else if (i>65) {
+               Console.WriteLine ("Price is: $ 9.99");
+           }
+            
+        }
+    }
+}
